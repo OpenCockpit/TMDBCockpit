26efc2aba5d1059dd434dc12a4ba40184a645830
xcentaurix <swmaniacster@gmail.com>
2026-08-27 12:33:46 +0000

    1.4.0

--- docs/install_feed.sh
+++ docs/install_feed.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo src/gz cockpit-all https://xcentaurix.github.io/Cockpit-Feed/packages/all > /etc/opkg/cockpit-feed-all.conf
+echo src/gz cockpit-all https://OpenCockpit.github.io/Cockpit-Feed/packages/all > /etc/opkg/cockpit-feed-all.conf
